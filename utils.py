589d451127169db8b097a385f86f6c0ce6a9172d
Vasily Dmitrochenko (LLC IPmatika) <v.dmitrochenko@ipmatika.com>
2026-06-19 12:33:07 +0300

    fix: remove dead imports get_regular_total_for_month and get_regular_payments_for_month from utils.py

--- utils.py
+++ utils.py
@@ -1,8 +1,8 @@
 from services.period_service import get_period, get_next_income_date, get_period_dates
 from services.regular_service import (
     get_regular_total, get_regular_payments_filtered, get_regular_payments_for_period,
-    get_regular_total_for_month, get_unpaid_regular_payments, get_regular_payments_until_date,
-    get_regular_payments_after_date, get_regular_payments_for_month, get_paid_regular_payments_this_month,
+    get_unpaid_regular_payments, get_regular_payments_until_date,
+    get_regular_payments_after_date, get_paid_regular_payments_this_month,
     get_due_regular_payments, apply_regular_payments,
 )
 from services.planning_service import get_planning_data
